3f7cff0a85b0ef8e2c9f123de854f3f94de809b4
Affandi Pratama <60037959+afanditama12@users.noreply.github.com>
2022-05-10 22:46:33 +0700

    tambah repository untuk project UTS ALPRO 2

---
+++ src/uts/No1BagianB.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uts;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lenovo
+ */
+public class No1BagianB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        testTampilanProgram();
+
+    }
+
+    public static void kodePelanggan(String kode) {
+        if (kode.equals("R")) {
+            System.out.println("Kode pelanggan anda adalah Reguler");
+        } else if (kode.equals("C")) {
+            System.out.println("Kode pelanggan anda adalah Paket Spesial");
+        } else if (kode.equals("V")) {
+            System.out.println("Kode pelanggan anda adalah VIP");
+        }
+    }
+
+    public static void testKodePelanggan() {
+        kodePelanggan("V");
+    }
+
+    public static void pemakaianData(int kuota, String kode) {
+        if (kuota <= 50) {
+            if (kode.equals("R")) {
+                int valueFirst = 2000;
+                System.out.println("Biaya yang harus di bayar : " + valueFirst);
+            }
+            if (kode.equals("C")) {
+                int valueSecond = 5000;
+                System.out.println("Biaya yang harus di bayar : " + valueSecond);
+            }
+            if (kode.equals("V")) {
+                int valueThird = 7500;
+                System.out.println("Biaya yang harus di bayar : " + valueThird);
+            }
+        } else if (kuota >= 51 && kuota <= 80) {
+            if (kode.equals("R")) {
+                int valueFirst = 4500;
+                System.out.println("Biaya yang harus di bayar : " + valueFirst);
+            }
+            if (kode.equals("C")) {
+                int valueSecond = 7000;
+                System.out.println("Biaya yang harus di bayar : " + valueSecond);
+            }
+            if (kode.equals("V")) {
+                int valueThird = 10000;
+                System.out.println("Biaya yang harus di bayar : " + valueThird);
+            }
+        } else if (kuota >= 80) {
+            if (kode.equals("R")) {
+                int valueFirst = 6500;
+                System.out.println("Biaya yang harus di bayar : " + valueFirst);
+            }
+            if (kode.equals("C")) {
+                int valueSecond = 10000;
+                System.out.println("Biaya yang harus di bayar : " + valueSecond);
+            }
+            if (kode.equals("V")) {
+                int valueThird = 15000;
+                System.out.println("Biaya yang harus di bayar : " + valueThird);
+            }
+        } 
+    }
+    
+    public static void testPemakaianData() {
+        pemakaianData(51, "V");
+    }
+    
+    public static void tampilanProgram() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukan kode pelanggan : ");
+        String inputKodePelanggan = scanner.next();
+        
+        if (inputKodePelanggan.equals("R")) {
+            kodePelanggan("R");
+        } else if (inputKodePelanggan.equals("C")) {
+            kodePelanggan("C");
+        } else if (inputKodePelanggan.equals("V")) {
+            kodePelanggan("V");
+        } else {
+            System.out.println("Maaf kode pelanggan yang anda masukan salah");
+            System.exit(0);
+        }
+        
+        System.out.print("Masukan jumlah pemakaian data : ");
+        int inputPemakaianData = scanner.nextInt();
+        
+        if (inputPemakaianData <= 50) {
+             pemakaianData(inputPemakaianData, inputKodePelanggan);
+        } else if (inputPemakaianData == 51 && inputPemakaianData <= 80) {
+            pemakaianData(inputPemakaianData, inputKodePelanggan);
+        } else if (inputPemakaianData >= 80) {
+            pemakaianData(inputPemakaianData, inputKodePelanggan);
+        }
+    }
+    
+    public static void testTampilanProgram() {
+        tampilanProgram();
+    }
+}
---
+++ src/uts/No2BagianA.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uts;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lenovo
+ */
+public class No2BagianA {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("banyaknya data rekursif = ");
+        int inputDataRekursif = scanner.nextInt();
+
+        int hasil = rekursif(inputDataRekursif);
+        System.out.println("\n" + "total operasi data rekursif = " + hasil);
+    }
+
+    public static int rekursif(int value) {
+        int S, x;
+
+        x = (3 * value) - 1;
+
+        if (value == 1) {
+            S = x;
+        } else {
+            S = x + (rekursif(value - 1));
+
+        }
+        System.out.print(x + " ");
+        return (S);
+    }
+}
